d78d90e914cb3cb29588dff0ce51fce325b4beef
gvytautas <gvytautas90@gmail.com>
2022-12-08 17:50:51 +0100

    lesson 23

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 """
--flask-project-structure
--config.py
--forms.py
--views.py
--models.py
--run.py
+-flask-application-deployment
+-linux server
+-nginx (reverse proxy)
+-gunicorn (WSGI Web Server Gateway Interface)
+-supervisor
 """
 from flask_project import app
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
